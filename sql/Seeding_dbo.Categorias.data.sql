6b0dd3db465fd85c7252f36d365bd888a23a0026
Gabriel Junio <gabriel.junio@rocketmail.com>
2025-08-11 13:42:59 -0300

    adição das interfaces

---
+++ sql/Seeding_dbo.Categorias.data.sql
@@ -0,0 +1,3 @@
+﻿INSERT INTO [dbo].[Categorias] ([Id], [Nome], [Codigo]) VALUES (N'47f5d8aa-e308-40b8-9d64-0523a2ce2d43', N'Canecas', 101)
+INSERT INTO [dbo].[Categorias] ([Id], [Nome], [Codigo]) VALUES (N'50dc4c1a-e238-4e42-8678-075189b71d1c', N'Camisetas', 100)
+INSERT INTO [dbo].[Categorias] ([Id], [Nome], [Codigo]) VALUES (N'd0584ce0-cbb9-4496-844d-f4ddce3ecad0', N'Adesivos', 102)
--- src/NerdStore.WebApp.MVC/Program.cs
+++ src/NerdStore.WebApp.MVC/Program.cs
@@ -22,9 +22,17 @@ builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.Requ
 
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddAutoMapper(c => c.AddMaps(typeof(AutoMappingProfileDomainToView), typeof(AutoMappingProfileViewToDomain)));
+var autoMapperAndMediatRLicenseKey = "eyJhbGciOiJSUzI1NiIsImtpZCI6Ikx1Y2t5UGVubnlTb2Z0d2FyZUxpY2Vuc2VLZXkvYmJiMTNhY2I1OTkwNGQ4OWI0Y2IxYzg1ZjA4OGNjZjkiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2x1Y2t5cGVubnlzb2Z0d2FyZS5jb20iLCJhdWQiOiJMdWNreVBlbm55U29mdHdhcmUiLCJleHAiOiIxNzg2NDA2NDAwIiwiaWF0IjoiMTc1NDkyOTUzOCIsImFjY291bnRfaWQiOiIwMTk4OTlmMzY4NDU3YzI3OTliODQ4YzZiOTRlMTI1NCIsImN1c3RvbWVyX2lkIjoiY3RtXzAxazJjejc5M213NnpjMG5hbWo1ZnhtZDV0Iiwic3ViX2lkIjoiLSIsImVkaXRpb24iOiIwIiwidHlwZSI6IjIifQ.Xj83QPw2dXhYIDFvEz12Egkn54oKft9PQWye9CFrmsB_JyFzUzrbrry1jWYq7ROstRFyReV1GbgdZ6ajffVsFrWBOk3Hevz0t2Mu1M3Es81BO-R03vJexAzWppHSuuR_j3KGlc0f84Qq_G0H6fgVpCQVmlEFymLRJMeZ6VkorxQLR9msN_PWyPLQKrH8FOkP4QL7Z0XEIO2OsuvzaatiYaAHpvK_Tsf478FxIfDa3_w8lY7hyptEIWMr12nl712goxhz0XVRxbUCcIXGPvTLX4u0i-Yb-3aAKt1dWJPC8zFeJWKOSUx2QbHHvQ3kdPX3sgB44mBReYPNMYwPfkoKCA";
 
-builder.Services.AddMediatR(c => c.RegisterServicesFromAssemblyContaining<Program>());
+builder.Services.AddAutoMapper(c => {
+    c.AddMaps(typeof(AutoMappingProfileDomainToView), typeof(AutoMappingProfileViewToDomain));
+    c.LicenseKey = autoMapperAndMediatRLicenseKey;
+});
+
+builder.Services.AddMediatR(c => { 
+    c.RegisterServicesFromAssemblyContaining<Program>();
+    c.LicenseKey = autoMapperAndMediatRLicenseKey;
+});
 
 builder.Services.RegisterServices();
 
@@ -51,7 +59,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
+    pattern: "{controller=Vitrine}/{action=Index}/{id?}")
     .WithStaticAssets();
 
 app.MapRazorPages()
