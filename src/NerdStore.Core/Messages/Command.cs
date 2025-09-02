9171d119f7a6473b2b191606e88c6084be973884
Gabriel Junio <gabriel.junio@rocketmail.com>
2025-09-02 13:27:31 -0300

    Adição dos comando para Command Handler

---
+++ src/NerdStore.Core/Messages/Command.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation.Results;
+using MediatR;
+
+namespace NerdStore.Core.Messages
+{
+    public class Command : Message, IRequest<bool>
+    {
+        public DateTime TimeStamp { get; private set; }
+
+        public ValidationResult ValidationResult { get; protected set; }
+
+        protected Command() 
+        {
+            TimeStamp = DateTime.Now;
+        }
+
+        public virtual bool EhValido()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- src/NerdStore.WebApp.MVC/Setup/DependencyInjection.cs
+++ src/NerdStore.WebApp.MVC/Setup/DependencyInjection.cs
@@ -5,6 +5,7 @@ using NerdStore.Catalogo.Data.Repository;
 using NerdStore.Catalogo.Domain;
 using NerdStore.Catalogo.Domain.Events;
 using NerdStore.Core.Bus;
+using NerdStore.Vendas.Application.Commands;
 
 namespace NerdStore.WebApp.MVC.Setup
 {
@@ -12,14 +13,19 @@ namespace NerdStore.WebApp.MVC.Setup
     {
         public static void RegisterServices(this IServiceCollection services)
         {
+            //Domain Bus (mediator)
             services.AddScoped<IMediatrHandler, MediatrHandler>();
 
+            //Catalogo
             services.AddScoped<IProdutoRepository, ProdutoRepository>();
             services.AddScoped<IProdutoAppService, ProdutoAppService>();
             services.AddScoped<IEstoqueService, EstoqueService>();
             services.AddScoped<CatalogoContext>();
 
             services.AddScoped<INotificationHandler<ProdutoAbaixoEstoqueEvent>, ProdutoEventHandler>();
+
+            // Vendas
+            services.AddScoped<IRequestHandler<AdicionarItemPedidoCommand, bool>, PedidoCommandHandler>();
         }
     }
 }
